47da0574b6fa33fb7ef4ee9a93ed0196e220bfcc
imanoliri <imanol.irizar13@gmail.com>
2024-11-17 11:32:06 +0100

    fix deploying netlify function

--- functions/logFeedback.js
+++ functions/logFeedback.js
@@ -1,5 +1,3 @@
-// functions/logFeedback.js
-
 exports.handler = async (event) => {
     try {
         // Parse the feedback data from the request body
@@ -8,12 +6,15 @@ exports.handler = async (event) => {
         // Log the feedback data to the Netlify function logs
         console.log("Received feedback data:", feedbackData);
 
+        // Respond with a success message
         return {
             statusCode: 200,
             body: JSON.stringify({ message: 'Feedback logged successfully!' })
         };
     } catch (error) {
         console.error('Error logging feedback:', error);
+
+        // Respond with an error message
         return {
             statusCode: 500,
             body: JSON.stringify({ error: 'Failed to log feedback.' })
