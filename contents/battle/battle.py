cb49152bb34abded442e9e86c8c2ef60ddef8e48
imanoliri <imanol.irizar13@gmail.com>
2024-12-05 16:09:12 +0100

    fix

--- contents/battle/battle.py
+++ contents/battle/battle.py
@@ -253,8 +253,6 @@ if __name__ == "__main__":
     battle_dirs = [f for f in os.listdir(battles_path) if os.path.isdir(os.path.join(battles_path, f))]
     for battle_dir in battle_dirs:
         battle_name = os.path.basename(battle_dir)
-        if battle_name != "battle_1":
-            continue
         print(f"{battle_name}...")
         generate_battle_autodata(battles_path, battle_name)
         print(f"\t{battle_name}\n")
